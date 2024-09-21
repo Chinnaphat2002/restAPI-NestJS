efd84bc819cd947e5dbd708774c16c5995a21f21
Chinnaphat <autt.chinnaphat@gmail.com>
2024-09-21 22:44:53 +0700

    crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,39 @@
+import { PrismaService } from './../prisma.service';
 import { User } from './entities/user.entity';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { FORBIDDEN_MESSAGE } from '@nestjs/core/guards';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    const users = await this.prisma.user.create({
-      data : {
-        username : createUserDto.username,
-        email : createUserDto.email,
-        password : createUserDto.password,
-        role : createUserDto.role,
+    const saltRounds = 10
+    const hash = await bcrypt.hashSync(createUserDto.password, saltRounds);
+    const isUser = await this.prisma.user.findFirst({
+      where: {
+        username : createUserDto.username
       }
+      
     })
-    return users
+    if(isUser){
+      throw new HttpException('Username already exit', HttpStatus.FORBIDDEN)
+    }else{
+      const users = await this.prisma.user.create({
+        data : {
+          username : createUserDto.username,
+          email : createUserDto.email,
+          password : hash,
+          role : createUserDto.role,
+        }
+      })
+      return users
+    }
+
+    
   }
 
   async findAll() {
@@ -32,20 +48,41 @@ export class UsersService {
     })
     return users
   }
-
   async update(id: number, updateUserDto: UpdateUserDto) {
-    const users = await this.prisma.user.update({
-      where : {
-        id : Number(id)
-      },
-        data : {
-        username : updateUserDto.username,
-        email : updateUserDto.email,
-        password : updateUserDto.password,
-        role : updateUserDto.role,
+    const saltRounds = 10
+    const hash = await bcrypt.hashSync(updateUserDto.password, saltRounds);
+    const isUser = await this.prisma.user.findFirst({
+      where :{
+        username: updateUserDto.username
       }
     })
-    return users
+    if(isUser){
+      const matchPassword = await bcrypt.compareSync(
+        updateUserDto.passwordOld,
+        isUser.password,
+      )
+      if(matchPassword){
+        const users = await this.prisma.user.update({
+          where : {
+            id : Number(id)
+          },
+            data : {
+            username : updateUserDto.username,
+            email : updateUserDto.email,
+            password : hash,
+            role : updateUserDto.role,
+          }
+        })
+        return users
+      }else{
+        throw new HttpException('Password Not Match',HttpStatus.FORBIDDEN)
+      }
+      
+    }else{
+      throw new HttpException('User not found',HttpStatus.FORBIDDEN )
+    }
+
+    
   }
 
   async remove(id: number) {
